0f7b40b6a62a87cf579e9f6517643d1108830f6b
Administrator <Administrator@WIN-GD0176P2P9B>
2016-07-15 04:18:38 +0530

    - Chapter-1 first example working with java based configuration

---
+++ MyBeginingSpringProjects/spring-book-ch2/src/main/java/com/wiley/beginning/spring/ch2/ex01/Ch2BeanConfiguration.java
@@ -0,0 +1,28 @@
+package com.wiley.beginning.spring.ch2.ex01;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is the java base configuration in spring.
+ * 
+ * @author Administrator
+ *
+ */
+@Configuration
+public class Ch2BeanConfiguration {
+
+	@Bean
+	public AccountService accountService() {
+		AccountServiceImpl bean = new AccountServiceImpl();
+		bean.setAccountDao(accountDao());
+		return bean;
+	}
+
+	@Bean
+	public AccountDao accountDao() {
+		AccountDaoInMemoryImpl bean = new AccountDaoInMemoryImpl();
+		// depedencies of accountDao bean will be injected here...
+		return bean;
+	}
+}
---
+++ MyBeginingSpringProjects/spring-book-ch2/src/main/java/com/wiley/beginning/spring/ch2/ex01/Main.java
@@ -0,0 +1,26 @@
+package com.wiley.beginning.spring.ch2.ex01;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				Ch2BeanConfiguration.class);
+
+		/** 
+		 * The method name is accepted as the bean name by default.
+		 * Hence, the first argument in the getBean method just below is the 
+		 * name of the method that getting passed to get the bean.
+		 * 
+		 * By default, each bean has a single instance, which is called singleton scope.
+		 */
+		AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+		System.out.println("Before money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+		accountService.transferMoney(1, 2, 5.0);
+		System.out.println("After money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+	}
+}
